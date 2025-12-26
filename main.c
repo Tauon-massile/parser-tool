4927e857e57391a63919aa1f677d6435fe35b0d7
Tauon-massile <709243566@qq.com>
2025-12-26 21:24:32 +0800

    linecount分支：添加文件行统计功能

--- main.c
+++ main.c
@@ -7,8 +7,8 @@ int main() {
     printf("字符串长度：%d\n", len);
     
     // 分支功能：行统计
-    // int lines = count_lines("test.txt");
-    // printf("文件行数：%d\n", lines);
+    int lines = count_lines("test.txt");
+    printf("文件行数：%d\n", lines);
     
     return 0;
 }
--- parser.c
+++ parser.c
@@ -8,3 +8,21 @@ int parse_strlen(const char *str) {
     }
     return strlen(str);
 }
+
+// 行统计功能
+int count_lines(const char *filename) {
+    if (filename == NULL) {
+        return -1;
+    }
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        return -2;
+    }
+    int lines = 0;
+    char buf[1024];
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+        lines++;
+    }
+    fclose(fp);
+    return lines;
+}
